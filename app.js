cb521e26b0a5757de2b0016ba0e0381faf1b51f5
sakshibhandari <bhandarisakshi04@gmail.com>
2021-03-20 15:15:27 +0530

    modified files

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 
+
 const app = express()
 
 app.use(cors())
-const port = 8000
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => res.json({ data: 'secret data' }))
 
-app.listen(port, () => console.log(`example app listening at http://localhost:${port}`))
+app.get("/", (req, res) => {
+    res.render('index', { title: 'test node' });
+})
+
+
+app.listen(port, () => {
+    console.log("server listening to port " + port);
+});
