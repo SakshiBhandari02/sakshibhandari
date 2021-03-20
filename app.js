15098aa414a8f3c5049036542bb11bfb5d16ec01
sakshibhandari <bhandarisakshi04@gmail.com>
2021-03-20 15:39:28 +0530

    etc

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express')
-const cors = require('cors')
-
-
 const app = express()
-
-app.use(cors())
 const port = process.env.PORT || 8000;
 
 
 app.get("/", (req, res) => {
-    res.render('index', { title: 'test node' });
+    res.send("hello from the express")
 })
 
 
